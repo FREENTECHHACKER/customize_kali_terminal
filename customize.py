3532a39777a784b2b742332cfef3794c9de2d71c
HIMANSHU SINHA <66861820+FREENTECHHACKER@users.noreply.github.com>
2020-07-06 19:12:03 +0530

    Create customize.py

---
+++ customize.py
@@ -0,0 +1,5 @@
+import pyfiglet
+result = pyfiglet.figlet_format("FREENTECH HACKER", font="slant")
+print(result)
+print("          \033[1;34;40m Author \033[1;33;40mFREENTECH HACKER\033[0m")
+print("")
